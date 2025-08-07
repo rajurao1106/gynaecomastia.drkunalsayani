448212d5981d5909977b7abf4606f319c2f3f52c
rajurao <rajurao1107@gmail.com>
2025-08-07 13:04:35 +0530

    commit changes

--- src/app/components/homepage/Hero.jsx
+++ src/app/components/homepage/Hero.jsx
@@ -7,13 +7,12 @@ export default function Hero() {
     <div>
       {/* Hero Section */}
       <section className="w-full flex justify-center max-lg:pt-28 items-center bg-blue-50 px-4 py-20 pt-38  text-center">
-        <div className="w-full flex flex-col lg:flex-row items-center justify-between max-w-[1100px] gap-8">
-          <div className="text-left max-w-xl max-lg:text-center">
+        <div className="w-full flex flex-col lg:flex-row items-center justify-between max-w-[1200px] gap-8">
+          <div className="text-left max-w-2xl max-lg:text-center">
             <h1 className="text-4xl md:text-5xl font-bold text-black mb-4">
-              Gynecomastia Surgery by Dr. Kunal Sayani Restore Your Masculine
-              Chest with Confidence
+              Gynecomastia Surgery by <br /> Dr. Kunal Sayani Restore Your Masculine Chest with Confidence
             </h1>
-            <p className="text-lg md:text-xl text-gray-900 mb-6">
+            <p className="text-base md:text-lg text-gray-900 mb-6">
               Say goodbye to male breast enlargement with advanced, scarless
               gynecomastia surgery by Dr. Kunal Sayani — leading plastic surgeon
               in Mumbai. Experience USFDA-approved technology, same-day
